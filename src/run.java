0a3a97ff09a8cd2fba8b2aa1188f4652d6d03dd0
William <109118303+Ww1ll@users.noreply.github.com>
2023-02-02 18:08:30 -0300

    Add files via upload

---
+++ src/run.java
@@ -0,0 +1,15 @@
+public class run {
+    public static void main(String[] args) {
+
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        m.setPeso(35.3f);
+        m.setCorPelo("Marrom");
+
+        m.locomover();
+        a.locomover();
+    }
+}
